7e3f75fb40c99f13310628cb43267eac53276ef5
James Paing <paingoomaw007@gmail.com>
2025-09-29 13:48:01 +0700

    fix: swagger specs type

--- src/lib/swagger.ts
+++ src/lib/swagger.ts
@@ -2,9 +2,10 @@
 import fs from 'fs';
 import yaml from 'js-yaml';
 import path from 'path';
+import { JsonObject } from 'swagger-ui-express';
 
 // Update to use 'swagger' folder and 'index.yaml' file
 const yamlFilePath = path.join(process.cwd(), 'swagger', 'openapi.yaml');
 const yamlFile = fs.readFileSync(yamlFilePath, 'utf8');
 
-export const specs = yaml.load(yamlFile);
+export const specs = yaml.load(yamlFile) as JsonObject;
