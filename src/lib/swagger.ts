4293270fb3512bab0e2eb28609bcb92dc37dfc1f
James Paing <paingoomaw007@gmail.com>
2025-10-01 12:22:36 +0700

    refactor: removed unnecessary swagger packages

--- src/lib/swagger.ts
+++ src/lib/swagger.ts
@@ -1,10 +1,8 @@
-// lib/swagger.js
 import fs from 'fs';
 import yaml from 'js-yaml';
 import path from 'path';
 import { JsonObject } from 'swagger-ui-express';
 
-// Update to use 'swagger' folder and 'index.yaml' file
 const yamlFilePath = path.join(process.cwd(), 'swagger', 'openapi.yaml');
 const yamlFile = fs.readFileSync(yamlFilePath, 'utf8');
 
